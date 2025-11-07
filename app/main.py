c0895d5688d186536dd3984e15fcc902f54d0b50
AbuUndercover <deine@email.de>
2025-11-07 19:55:08 +0100

    Fix for Render deploy

--- app/main.py
+++ app/main.py
@@ -120,4 +120,6 @@ def export_auftraege():
 # App-Startpunkt
 # ------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Nur lokal starten, nicht auf Render
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
